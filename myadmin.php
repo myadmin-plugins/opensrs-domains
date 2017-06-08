02dfca3505ae7688c400911ec64f6d642eb88a23
Joe Huss <detain@interserver.net>
2017-06-08 00:59:30 -0400

    plugin definition updates

--- myadmin.php
+++ myadmin.php
@@ -8,17 +8,17 @@ return [
 	'name' => 'Opensrs Domains',
 	'description' => 'Allows selling of Opensrs Server and VPS License Types.  More info at https://www.netenberg.com/opensrs.php',
 	'help' => 'It provides more than one million end users the ability to quickly install dozens of the leading open source content management systems into their web space.  	Must have a pre-existing cPanel license with cPanelDirect to purchase a opensrs license. Allow 10 minutes for activation.',
-	'module' => 'licenses',
+	'module' => 'domains',
 	'author' => 'detain@interserver.net',
 	'home' => 'https://github.com/detain/myadmin-opensrs-domains',
 	'repo' => 'https://github.com/detain/myadmin-opensrs-domains',
 	'version' => '1.0.0',
-	'type' => 'licenses',
+	'type' => 'service',
 	'hooks' => [
 		/*'function.requirements' => ['Detain\MyAdminOpensrs\Plugin', 'Requirements'],
-		'licenses.settings' => ['Detain\MyAdminOpensrs\Plugin', 'Settings'],
-		'licenses.activate' => ['Detain\MyAdminOpensrs\Plugin', 'Activate'],
-		'licenses.change_ip' => ['Detain\MyAdminOpensrs\Plugin', 'ChangeIp'],
+		'domains.settings' => ['Detain\MyAdminOpensrs\Plugin', 'Settings'],
+		'domains.activate' => ['Detain\MyAdminOpensrs\Plugin', 'Activate'],
+		'domains.change_ip' => ['Detain\MyAdminOpensrs\Plugin', 'ChangeIp'],
 		'ui.menu' => ['Detain\MyAdminOpensrs\Plugin', 'Menu'] */
 	],
 ];
