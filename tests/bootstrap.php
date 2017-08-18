3ee2486ffe9704d7a85d834db636bad6fe8391b7
Joe Huss <detain@interserver.net>
2017-08-18 18:47:00 -0400

    updates adding in opensrs defines

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,2 +1,6 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
+define('OPENSRS_USERNAME', getenv('OPENSRS_USERNAME'));
+define('OPENSRS_PASSWORD', getenv('OPENSRS_PASSWORD'));
+define('OPENSRS_KEY', getenv('OPENSRS_KEY'));
+define('OPENSRS_TEST_KEY', getenv('OPENSRS_TEST_KEY'));
